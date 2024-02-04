dd8eeaafa82115c2510ef00380a718697eb19238
0xblessed <alemube2002@gmail.com>
2024-02-04 22:58:41 +0100

    Makefila & .h

---
+++ push_swap.h
@@ -0,0 +1,16 @@
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include    "./42-Libft/libft.h"
+# include    <stdio.h>
+# include    <stdlib.h>
+
+// Structs
+typedef struct STRUCT_PLANTILLA
+{
+	int hombroS_como_cocosss;
+}				STRUCT_PLANTILLA;
+
+//Funciones
+
+#endif
