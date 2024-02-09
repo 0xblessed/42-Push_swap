7177b2a53eb36d19e4724104c9ec1ca6cf25dd52
0xblessed <alemube2002@gmail.com>
2024-02-09 15:24:31 +0100

    Control de entrada v1

--- push_swap.c
+++ push_swap.c
@@ -16,7 +16,7 @@ void insertar_stack(NODE** root, int value)
         *root = new_node;
         return ; 
     }
-    NODE* curr = *root;;
+    NODE* curr = *root;
     while(curr->next != NULL)
     {
         curr = curr->next; 
@@ -26,20 +26,50 @@ void insertar_stack(NODE** root, int value)
 
 void inicializar_stack(NODE *root, char **argv, int argc)
 {
-    root->x = ft_atoi(argv[1]);
-    insertar_stack(&root, ft_atoi(argv[2]));
-    insertar_stack(&root, ft_atoi(argv[3]));
+    int i = 2;
+    int j = 1;
+    if (argc == -1)
+    {
+        //bucle del split
+        root->x = ft_atoi(argv[0]);
+        while (argv[j] != NULL)
+        {
+            insertar_stack(&root, ft_atoi(argv[j]));
+            j++;
+        }
+        
+    }
+    else
+    {
+        root->x = ft_atoi(argv[1]);
+    
+        while(i < argc)
+        {
+            insertar_stack(&root, ft_atoi(argv[i]));
+            i++;
+        }
+    }
+    
     
 }
 
-int main(int argc, char **argv)
+/*int main(int argc, char **argv)
 {
-    if (argc > 2)
+    if (argc < 1)
     {
         NODE *stackA = malloc(sizeof(NODE));
         NODE *stackB = malloc(sizeof(NODE));
     
-        inicializar_stack(stackA, argv, argc);
+        
+        if (argc == 2)
+        {
+            inicializar_stack(stackA, ft_split(argv[1], ' '), -1);
+        }
+        else
+        {
+            inicializar_stack(stackA, argv, argc);
+        }
+        
         //Printa el stack
         NODE *curr = malloc(sizeof(NODE));
         curr = stackA;
@@ -54,4 +84,28 @@ int main(int argc, char **argv)
     {
         write(1, "Error", 5);
     }
+}*/
+int main(int argc, char **argv) {
+    if (argc > 1) {
+        NODE *stackA = malloc(sizeof(NODE));
+        NODE *stackB = malloc(sizeof(NODE));
+
+        if (argc == 2) {
+            inicializar_stack(stackA, ft_split(argv[1], ' '), -1);
+        } else {
+            inicializar_stack(stackA, argv, argc);
+        }
+
+        // Imprime el stack
+        NODE *curr = stackA;
+
+        while (curr != NULL) {
+            printf("%d\n", curr->x);
+            curr = curr->next;
+        }
+    } else {
+        write(1, "Error\n", 6);
+    }
+
+    return 0;
 }
