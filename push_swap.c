337ee28b43305e028c8d0d452339ae77945d59cc
0xblessed <alemube2002@gmail.com>
2024-02-10 16:36:37 +0100

    Control errores v2 - Mirar duplicados - Mirar limites

--- push_swap.c
+++ push_swap.c
@@ -56,13 +56,24 @@ void inicializar_stack(NODE *root, char **argv, int argc)
 int main(int argc, char **argv) {
 
     int alpha = 0;
+    int j = 0;
+    int i = 1;
+    int found_alpha = 0;
 
     if (argc > 1) {
+        
+        if (ft_strlen(argv[1]) == 0)
+        {
+            write(1, "Error\n", 6);
+            return 0;
+        }
+        
         NODE *stackA = malloc(sizeof(NODE));
         NODE *stackB = malloc(sizeof(NODE));
 
-        if (argc == 2) {
-            int j = 0;
+        if (argc == 2)
+        {
+            j = 0;
             while(argv[1][j] != '\0')
             {
                 alpha = ft_isalpha(argv[1][j]);
@@ -74,22 +85,44 @@ int main(int argc, char **argv) {
                 write(1, "Error\n", 6);
             else
                 inicializar_stack(stackA, ft_split(argv[1], ' '), -1);
-        } else {
-            // falta implementar control errores de caracteres en argumentos sueltos
-            inicializar_stack(stackA, argv, argc);
         }
-
-        // Imprime el stack
-        if (!alpha)
+        else
         {
-            NODE *curr = stackA;
-
-            while (curr != NULL)
+            // falta implementar control errores de caracteres en argumentos sueltos
+            /////////////////////
+            while (argv[i] != NULL)
             {
-                printf("%d\n", curr->x);
-                curr = curr->next;
+                j = 0;
+                while (argv[i][j] != '\0') {
+                    if (ft_isalpha(argv[i][j]))
+                    {
+                        alpha = 1;
+                        break;
+                    }
+                    j++;
+                }
+                if (alpha)
+                    break;
+                i++;
             }
+            if (alpha)
+                write(1, "Error\n", 6);
+            else 
+                inicializar_stack(stackA, argv, argc);
+            /////////////////
         }
+
+                    // Imprime el stack
+                        if (!alpha)
+                        {
+                            NODE *curr = stackA;
+
+                            while (curr != NULL)
+                            {
+                                printf("%d\n", curr->x);
+                                curr = curr->next;
+                            }
+                        }
     } else {
         write(1, "Error\n", 6);
     }
